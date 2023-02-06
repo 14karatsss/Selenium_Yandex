8db376ac79aa76cf945b719e1887fd25aa56fb4c
14karatsss <14karatsss14@gmail.com>
2023-02-06 15:18:38 +0600

    Add project

--- test_case/test_yandex_search.py
+++ test_case/test_yandex_search.py
@@ -5,11 +5,8 @@ def test_yandex_search(browser):
     yandex_main_page = SearchHelper(browser)
     yandex_main_page.go_to_site()
     yandex_main_page.enter_word("Hello")
-    #time.sleep(2)
     yandex_main_page.click_on_the_search_button()
-    time.sleep(2)
     elements = yandex_main_page.check_navigation_bar()
-    time.sleep(2)
-    assert "anime" in elements
+    assert "Картинки" in elements
 
 
